ca1c9c45267c066eff85c277128a4ff0257bb790
Xine75 <cjdoza@gmail.com>
2022-08-03 10:09:50 -0500

    landing page styling

---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="posts-page">
+    Posts page
+  </div>
+</template>
+
+<style scoped>
+.posts-page{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+</style>

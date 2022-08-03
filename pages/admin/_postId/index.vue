bd7d7a147c5f177fa038d3913c8906abf8ec2275
Xine75 <cjdoza@gmail.com>
2022-08-03 14:06:09 -0500

    edit post

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="admin-post-page">
+  <section class="update-form">
+  <AdminPostForm :post="loadedPost" />
+  </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '~/components/Admin/AdminPostForm'
+export default {
+  components: {AdminPostForm},
+  data(){
+    return {
+      loadedPost: {
+        author: 'Christine',
+        title: 'My New Lipstick Obsession',
+        content: 'I only want to wear bright colors and gloss',
+        thumbnailLink: 'https://www.seekpng.com/png/full/474-4740034_pop-art-lips.png'
+      }
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
